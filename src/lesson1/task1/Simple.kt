2433911f87e3730631588006423ca21090ba0dbd
bykAleks <plastation4.09@gmail.com>
2020-09-10 00:47:59 +0300

    Решение всех задач

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -135,4 +135,11 @@ fun accountInThreeYears(initial: Int, percent: Int): Double {
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
+fun numberRevert(number: Int): Int {
+    var numberRevers = number % 10
+    numberRevers *= 10
+    numberRevers += (number / 10) % 10
+    numberRevers *= 10
+    numberRevers += number / 100
+    return numberRevers
+}
