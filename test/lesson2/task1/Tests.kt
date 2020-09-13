35f5bee0b6c789d7e3e0c1880cf3edf52883a62b
bykAleks <plastation4.09@gmail.com>
2020-09-14 01:33:41 +0300

    Все задачи 2 лекции

--- test/lesson2/task1/Tests.kt
+++ test/lesson2/task1/Tests.kt
@@ -68,6 +68,7 @@ class Tests {
         assertEquals(1, rookOrBishopThreatens(2, 8, 6, 8, 1, 6))
         assertEquals(2, rookOrBishopThreatens(5, 4, 3, 7, 1, 8))
         assertEquals(3, rookOrBishopThreatens(1, 6, 7, 6, 3, 8))
+        assertEquals(1, rookOrBishopThreatens(1, 1, 1, 2, 2, 4))
     }
 
     @Test
