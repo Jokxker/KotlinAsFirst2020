918f125f1ed69cf5d422ecafd1f84104349d9885
bykAleks <plastation4.09@gmail.com>
2020-09-18 01:00:02 +0300

    lesson 2 task 2

--- test/lesson2/task2/Tests.kt
+++ test/lesson2/task2/Tests.kt
@@ -68,5 +68,6 @@ class Tests {
         assertFalse(brickPasses(6, 5, 4, 3, 6))
         assertTrue(brickPasses(3, 2, 1, 1, 2))
         assertFalse(lesson2.task2.brickPasses(2, 2, 1, 1, 1))
+        assertFalse(lesson2.task2.brickPasses(1000, 1, 934, 213, 752))
     }
 }
