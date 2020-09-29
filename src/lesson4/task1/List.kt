97209d453d65bddc5f895ffe739f75c6cf36246e
bykAleks <plastation4.09@gmail.com>
2020-09-29 23:00:16 +0300

    lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -128,7 +128,7 @@ fun abs(v: List<Double>) = sqrt(v.sumByDouble { it.pow(2) })
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>) = if (list.isEmpty()) 0.0 else list.sum() / list.size
 
 /**
  * Средняя (3 балла)
