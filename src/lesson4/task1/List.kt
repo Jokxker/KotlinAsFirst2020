1348bc4968904cddc2130dadcd6613b795e89c52
bykAleks <plastation4.09@gmail.com>
2020-10-01 21:58:15 +0300

    lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -240,10 +240,10 @@ fun factorizeToString(n: Int): String {
  */
 fun convert(n: Int, base: Int): List<Int> {
     var num = n
-    var b = 1
+    var b = 1L
     val countBase = mutableListOf<Int>()
     while (b <= n) {
-        countBase.add(b)
+        countBase.add(b.toInt())
         b *= base
     }
     val result = mutableListOf<Int>()
